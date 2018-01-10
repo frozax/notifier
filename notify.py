05e2abdbe048791fe8344dddcd4947457f597cc0
Francois Guibert <frozax@frozax.com>
2018-01-10 20:28:53 +0100

    cron option

--- notify.py
+++ notify.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import time
+from datetime import datetime
 import sys
 import argparse
 import importlib
@@ -12,6 +14,8 @@ def parse_args():
                         help="argument to source")
     parser.add_argument("--destination", "-d", type=str, default="stdout",
                        help="Where to send data")
+    parser.add_argument("--cron", type=str, default="",
+                        help="Notifier will be called every day at the time given (e.g: 1500)")
 
     return parser.parse_args()
 
@@ -19,6 +23,15 @@ def _err(s):
     print("FATAL ERROR")
     print(s)
     sys.exit(1)
+
+
+def call(source, dest):
+    try:
+        source_data = source.get_text()
+    except:
+        source_data = "FATAL ERROR: Could not get data\n%s" % sys.exc_info()[0].__name__
+
+    dest.send(source_data)
 
 if __name__ == '__main__':
     args = parse_args()
@@ -37,10 +50,19 @@ if __name__ == '__main__':
     except ImportError:
         _err("Could not import " + args.destination)
 
-    try:
-        source_data = source.get_text()
-    except:
-        source_data = "FATAL ERROR: Could not get data\n%s" % sys.exc_info()[0].__name__
-        raise
-
-    dest.send(source_data)
+    if args.cron:
+        cur_time = datetime.time(datetime.now())
+        cur_time_min = cur_time.minute + cur_time.hour * 60
+        wished_time_min = int(args.cron[:2]) * 60 + int(args.cron[2:])
+        initial_sleep = wished_time_min - cur_time_min
+        day_min = 24 * 60
+        if initial_sleep < 0:
+            initial_sleep += day_min
+        print("Initial sleep %d minutes" % initial_sleep)
+        time.sleep(initial_sleep * 60)
+        while True:
+            call(source, dest)
+            print("Sleep %d seconds" % (day_min * 60))
+            time.sleep(day_min * 60)
+    else:
+        call(source, dest)
